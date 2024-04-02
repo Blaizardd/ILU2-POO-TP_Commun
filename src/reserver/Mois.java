db2a8e27c5d221560f073161da0ab3c2e4c5119d
Jeff Bouche <bcj4864a@ensfsi.onmicrosoft.com>
2024-04-02 15:26:20 +0200

    modif 1 save

--- src/reserver/Mois.java
+++ src/reserver/Mois.java
@@ -1,9 +1,64 @@
 package reserver;
 
-public class Mois {
+public class CalendrierAnnuel{
+	public CalendrierAnnuel() {
+		Mois calendrier [] = new Mois[12];
+		calendrier[0] = new Mois("Janvier", 31);
+		calendrier[0] = new Mois("Février", 31);
+		calendrier[0] = new Mois("Mars", 31);
+		calendrier[0] = new Mois("Avril", 31);
+		calendrier[0] = new Mois("Mai", 31);
+		calendrier[0] = new Mois("Juin", 31);
+		calendrier[0] = new Mois("Juillet", 31);
+		calendrier[0] = new Mois("Aout", 31);
+		calendrier[0] = new Mois("Septembre", 31);
+		calendrier[0] = new Mois("Octobre", 31);
+		calendrier[0] = new Mois("Novembre", 31);
+		calendrier[0] = new Mois("Decmbre", 31);
 
-	public Mois() {
-		// TODO Auto-generated constructor stub
+		
 	}
+	
+	
+	
+	
+	
+	
+	
+	
+	public class Mois {
+		private String nom;
+		private int nbJours;
+		private boolean [] jours;
+		
+		public Mois(String nom, int nbJours) {
+			this.nom = nom;
+			boolean jours[] = new boolean[nbJours];
+		
+		
+		}
+	
+		public boolean estLibre(int jour) {
+			if (jours[jour-1] == true) {
+				return true;
+			}
+			else {
+				return false;
+			}
+		}
+		
+		public void reserver(int jour) {
+			///if (jours[jour-1] == null) {
+				//return -1; //FAIRE LE TRY EXCEPT
+			//}
+			if (estLibre(jour)) {
+				jours[jour-1] = false;
+			}
+			else {
+				System.out.println("deso ce jour n'est pas libre");
+				//faire l'exception IllegalStateException
+			}
 
+	}
+	
 }
